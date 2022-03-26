2c80ddce7c6e5350436735b298e3cb5ce29d71be
MACANNAdev <trevmc77@gmail.com>
2022-03-26 12:06:43 +0000

    Add removeCard class to  Tis when dissablecard functions is called, fix sasme card neing called twice bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,12 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+  if(lockBoard) return;
+  if(this === firstCard) return;
   this.classList.add('flip');
 
   if (!hasFlippedCard) {
@@ -25,18 +28,29 @@ function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
   isMatch ? disableCards() : unflipCards();
+
+ 
 }
 
 function disableCards() {
   firstCard.removeEventListener('click', flipCard);
   secondCard.removeEventListener('click', flipCard);
+
+   
+  firstCard.classList.add('removeCard');
+  secondCard.classList.add('removeCard');
+   
+
 }
 
+
 function unflipCards() {
-  setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
-  }, 1500);
+  lockBoard = true;
+  setTimeout(function () {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+      lockBoard = false;
+    }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
